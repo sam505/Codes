8e22260676ea4754c7441370b10cc373c8f50aa7
sam505 <mainasamuel505@gmail.com>
2023-06-17 21:25:26 +0300

    Add minimaxsum tests

--- minimaxsum_test.py
+++ minimaxsum_test.py
@@ -1,3 +1,21 @@
 from minimaxsum import *
 
 
+def test_miniMaxSum_one():
+    arr = [1, 3, 5, 7, 9]
+
+    actual = [16, 24]
+
+    calculated = miniMaxSum(arr)
+
+    assert actual == calculated
+
+
+def test_miniMaxSum_two():
+    arr = [1, 2, 3, 4, 5]
+
+    actual = [10, 14]
+
+    calculated = miniMaxSum(arr)
+
+    assert actual == calculated
