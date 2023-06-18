6931df61e7a920cebc127ad70a685c458fd6adc7
sam505 <mainasamuel505@gmail.com>
2023-06-18 22:51:49 +0300

    Update lonely integer test cases

---
+++ lonely_integer_test.py
@@ -0,0 +1,10 @@
+from lonely_integer import *
+
+def test_one_lonelyinteger(a):
+    a = [1, 2, 3, 4, 3, 2, 1]
+
+    actual = 4
+
+    calculated = lonelyinteger(a)
+
+    assert actual == calculated
