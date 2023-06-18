8b468d10553f24604329f21b12a3050868f75b72
sam505 <mainasamuel505@gmail.com>
2023-06-18 23:03:24 +0300

    update docstrings

--- lonely_integer.py
+++ lonely_integer.py
@@ -1,4 +1,12 @@
-def lonelyinteger(a):
+def lonelyinteger(a: list) -> int:
+    """Function that gets the lonely integer in a list
+
+    Args:
+        a (list): a list containing integers
+
+    Returns:
+        int: an interger that only appears once in the list
+    """
     for num in a:
         if a.count(num) == 1:
             return num
