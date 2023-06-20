ec45c9af52afd84e7be0bcbe5d3b990ddaacee1b
sam505 <mainasamuel505@gmail.com>
2023-06-21 01:40:20 +0300

    Add pangrams solution

--- pangrams.py
+++ pangrams.py
@@ -1,3 +1,11 @@
-def pangrams(s):
+def pangrams(s: str) -> str:
     # Write your code here
-    pass
+    s = s.lower()
+    alphabet = [chr(i) for i in range(97, 123)]
+
+    for char in alphabet:
+        if char not in s:
+            return "not pangram"
+    
+    return "pangram"
+    
