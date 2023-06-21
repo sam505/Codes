b34d1c9178221ef6fdb6d25fd7c05fadb943226e
sam505 <mainasamuel505@gmail.com>
2023-06-21 15:14:05 +0300

    Add test cases

--- reverse_alpha_test.py
+++ reverse_alpha_test.py
@@ -1,1 +1,17 @@
-from .sort_alpha import *
+from .reverse_alpha import *
+
+
+def test_one_sort_alpha():
+    s = "ab-cd"
+
+    actual = "dc-ba"
+
+    calculated = reverse_aplha(s)
+
+
+def test_two_sort_alpha():
+    s = "a-bC-dEf=ghIj!!"
+
+    actual = "j-Ih-gfE=dCba!!"
+
+    calculated = reverse_aplha(s)
