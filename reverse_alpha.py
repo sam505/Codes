f06ad14a82266103e01c562d8513f2cd50767277
sam505 <mainasamuel505@gmail.com>
2023-06-21 15:23:48 +0300

    Add reverse_alpha solution

--- reverse_alpha.py
+++ reverse_alpha.py
@@ -1,2 +1,9 @@
-def sort_aplha():
-    pass
+def reverse_aplha(s: str) -> str:
+
+    s_new = s[::-1]
+
+    s_new = [char for char in s_new if char.isalpha()]
+
+    [s_new.insert(i, s[i]) for i in range(len(s)) if not s[i].isalpha()]
+
+    return "".join(s_new)
