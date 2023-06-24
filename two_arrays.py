9357233f298599564da8391584a86089f452ce22
sam505 <mainasamuel505@gmail.com>
2023-06-24 20:11:55 +0300

    Add Birthdays solution

--- two_arrays.py
+++ two_arrays.py
@@ -1,3 +1,20 @@
 def twoArrays(k:int, A:list, B:list) -> str:
-    # Write your code here
-    pass
+    """_summary_
+
+    Args:
+        k (int): an integer
+        A (list): a list
+        B (list): a list
+
+    Returns:
+        str: a string (YES or NO)
+    """
+    
+    a_sorted = sorted(A, reverse=False)
+    b_sorted = sorted(B, reverse=True)
+
+    for i in range(len(A)):
+        if a_sorted[i] + b_sorted[i] < k:
+            return "NO"
+    
+    return "YES"
