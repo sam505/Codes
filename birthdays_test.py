0e41ad08ea770febe41881e84e4f056d2592a116
sam505 <mainasamuel505@gmail.com>
2023-06-25 22:48:51 +0300

    Add birthdays test cases

--- birthdays_test.py
+++ birthdays_test.py
@@ -11,3 +11,39 @@ def test_one_birthdays():
     calculated = birthday(s, d, m)
 
     assert actual == calculated
+
+
+def test_two_birthdays():
+    s = [1, 2, 1, 3, 2]
+    d = 3
+    m = 2
+
+    actual = 2
+
+    calculated = birthday(s, d, m)
+
+    assert actual == calculated
+
+
+def test_three_birthdays():
+    s = [1, 1, 1, 1, 1, 1]
+    d = 3
+    m = 2
+
+    actual = 0
+
+    calculated = birthday(s, d, m)
+
+    assert actual == calculated
+
+
+def test_four_birthdays():
+    s = [4]
+    d = 4
+    m = 1
+
+    actual = 1
+
+    calculated = birthday(s, d, m)
+
+    assert actual == calculated
