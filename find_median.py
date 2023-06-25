6dfbee27ddea31796fe039cc91f16c8bb95a5c26
sam505 <mainasamuel505@gmail.com>
2023-06-26 01:47:18 +0300

    Add find median solution

--- find_median.py
+++ find_median.py
@@ -1,3 +1,9 @@
 def findMedian(arr:list) -> int:
     # Write your code here
-    
+    arr = sorted(arr)
+    length = len(arr)
+
+    if length % 2 == 0:
+        return (arr[int(length/2)] + arr[int(len/2 - 1)])/2
+    else:
+        return arr[int(length/2)]
