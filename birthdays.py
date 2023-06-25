716d920a394d72b97e8391e93118650f6810f22b
sam505 <mainasamuel505@gmail.com>
2023-06-25 22:52:26 +0300

    Add birthdays solution

--- birthdays.py
+++ birthdays.py
@@ -1,3 +1,27 @@
-def birthday(s, d, m):
+def birthday(s:list, d:int, m:int) -> int:
+    """_summary_
+
+    Args:
+        s (list): a list
+        d (int): an integer, sum of segment
+        m (int): an integer, length of segment
+
+    Returns:
+        int: an integer
+    """
     # Write your code here
-    pass
+    pieces = 0
+
+    while len(s) >= m:
+        if sum(s[:m]) == d:
+            s.pop(0)
+            pieces += 1
+        else:
+            s.pop(0)
+    
+    return pieces
+
+
+
+        
+            
