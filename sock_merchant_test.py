d6183a58b823d10573d01bbc5470153e0aeb1119
sam505 <mainasamuel505@gmail.com>
2023-06-28 02:38:02 +0300

    Add socks_merchant test cases

--- sock_merchant_test.py
+++ sock_merchant_test.py
@@ -1,12 +1,23 @@
-def sockMerchant(n:int, ar:list) -> int:
-    """_summary_
+from sock_merchant import sockMerchant
 
-    Args:
-        n (int): number of socks
-        ar (list): socks available
 
-    Returns:
-        int: number of pairs available
-    """
-    # Write your code here
-    pass
+def test_one_sock_merchant():
+    n = 7
+    arr = [1, 2, 1, 2, 1, 3, 2]
+
+    actual = 2
+
+    calculated = sockMerchant(n, arr)
+
+    assert actual == calculated
+
+
+def test_two_sock_merchant():
+    n = 7
+    arr = [10, 20, 20, 10, 10, 30, 50, 10, 20]
+
+    actual = 3
+
+    calculated = sockMerchant(n, arr)
+
+    assert actual == calculated     
