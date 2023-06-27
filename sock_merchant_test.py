aa02868661effe29564ae512fd61fcab3f445274
sam505 <mainasamuel505@gmail.com>
2023-06-28 02:33:52 +0300

    Add socks_merchant challenge

---
+++ sock_merchant_test.py
@@ -0,0 +1,12 @@
+def sockMerchant(n:int, ar:list) -> int:
+    """_summary_
+
+    Args:
+        n (int): number of socks
+        ar (list): socks available
+
+    Returns:
+        int: number of pairs available
+    """
+    # Write your code here
+    pass
