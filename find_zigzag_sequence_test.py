a4de40255b894addcbf5462e603f9c36442c92a7
sam505 <mainasamuel505@gmail.com>
2023-06-30 11:50:05 +0300

    Add test case

--- find_zigzag_sequence_test.py
+++ find_zigzag_sequence_test.py
@@ -4,10 +4,18 @@ from find_zigzag_sequence import findZigZagSequence
 def test_one_find_zigzag_sequence():
     seq = [2, 3, 5, 1, 4]
 
-    actual = [1, 4, 5, 3, 2]
+    actual = [1, 2, 5, 4, 3]
 
     calculated = findZigZagSequence(seq, len(seq))
 
     assert actual == calculated
 
 
+def test_two_find_zigzag_sequence():
+    seq = [1, 2, 3, 4, 5, 6, 7]
+
+    actual = [1, 2, 3, 7, 6, 5, 4]
+
+    calculated = findZigZagSequence(seq, len(seq))
+
+    assert actual == calculated
