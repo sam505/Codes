77d7679c649ef4120ceadb9d8d0315f7842fa9bc
sam505 <mainasamuel505@gmail.com>
2023-07-02 16:13:11 +0300

    Add max_min solution

--- max_min.py
+++ max_min.py
@@ -1,3 +1,27 @@
-def maxMin(k, arr):
+def maxMin(k: int, arr: list) -> int:
+    """_summary_
+
+    Args:
+        k (int): number of elements to select
+        arr (list): an array of integers
+
+    Returns:
+        int: minimum possible unfairness
+    """
     # Write your code here
-    pass
+    min_unfairness = max(arr)
+    
+    arr = sorted(arr)
+    i = 0
+    while i <= len(arr) - k:
+        unfairness = max(arr[i:i+k]) - min(arr[i:i+k])
+        print(arr[i:i+k])
+
+        if unfairness < min_unfairness:
+            min_unfairness = unfairness
+
+        i += 1
+
+    return min_unfairness
+
+    
