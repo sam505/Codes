6da865d8ce83b2a715f72acf3c6292ddb2b7172b
sam505 <mainasamuel505@gmail.com>
2023-07-02 15:24:07 +0300

    Update tower breakers

--- tower_breakers.py
+++ tower_breakers.py
@@ -1,4 +1,13 @@
-def towerBreakers(n, m):
+def towerBreakers(n:int, m:int) -> int:
+    """Function to calculate the player who reduces towers first leaving the other with no chance to play
+
+    Args:
+        n (int): number of towers
+        m (int): length of each tower
+
+    Returns:
+        int: winner, either 1 or 2
+    """
     # Write your code here
     
     # create the towers and their initial height
